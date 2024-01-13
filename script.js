7cadf0fad8a9db9c0ec13368320150e0b5489e95
Navya <88023981+navysai1419@users.noreply.github.com>
2024-01-13 21:17:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    const developers = arr.filter((employee) => employee.profession === "developer");
+    const developerNames = developers.map((developer) => developer.name);
+    console.log(developerNames);
+  }
+  
+  function PrintDeveloperbyForEach() {
+    const developerNames = [];
+    arr.forEach((employee) => {
+      if (employee.profession === "developer") {
+        developerNames.push(employee.name);
+      }
+    });
+    console.log(developerNames);
+  }
+  
+  function addData() {
+    const newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newEmployee);
+    console.log(newEmployee);
+  }
+  
+  function removeAdmin() {
+    arr = arr.filter((employee) => employee.profession !== "admin");
+    console.log(arr);
+  }
+  
+  
+  function concatenateArray() {
+    const newArray = [
+      { id: 4, name: "Alice", age: "22", profession: "designer" },
+      { id: 5, name: "Bob", age: "25", profession: "manager" },
+      { id: 6, name: "Charlie", age: "28", profession: "engineer" }
+    ];
+  
+    const concatenatedArray = arr.concat(newArray);
+    console.log(concatenatedArray);
+  }
+  
